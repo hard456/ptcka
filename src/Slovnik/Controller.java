2f28a599faf031a6a42c6efc25a7f7a7a6133fee
honzaseda <honza.seda@email.cz>
2016-10-08 21:57:55 +0200

    Vybírání souborů pro nahrání slovníku

--- src/Slovnik/Controller.java
+++ src/Slovnik/Controller.java
@@ -1,4 +1,34 @@
 package Slovnik;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.layout.GridPane;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+import java.io.File;
+
 public class Controller {
+    @FXML
+    public Text statusText;
+    @FXML
+    public Button openNew;
+    @FXML
+    public Button openExisting;
+    @FXML
+    private GridPane gp;
+
+    public void open() {
+        Stage stage = (Stage) gp.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose a File");
+        FileChooser.ExtensionFilter extensionFilter=new FileChooser.ExtensionFilter("TXT", "*.txt");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+        //for showing the popup window to open file
+        File file=fileChooser.showOpenDialog(stage);
+        if(file != null) statusText.setText("Vybraný soubor: " + file.getName().toString());
+        else statusText.setText("Nebyl vybrán žádný soubor");
+    }
+
 }
--- src/Slovnik/Main.java
+++ src/Slovnik/Main.java
@@ -1,10 +1,13 @@
 package Slovnik;
 
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
 
 public class Main extends Application {
 
@@ -14,6 +17,14 @@ public class Main extends Application {
         primaryStage.setTitle("PT - Slovník");
         primaryStage.setScene(new Scene(root, 800, 600));
         primaryStage.show();
+
+        primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+            @Override
+            public void handle(WindowEvent e) {
+                Platform.exit();
+                System.exit(0);
+            }
+        });
     }
 
 
